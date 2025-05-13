973227a89b72cf20ca874de563dea16c5e6ac469
S25130 <s25130@pjwstk.edu.pl>
2025-05-13 20:06:51 +0200

    Update Doctor.cs

--- S25130/APBD_cw5/Models/Doctor.cs
+++ S25130/APBD_cw5/Models/Doctor.cs
@@ -1,6 +1,8 @@
 namespace APBD_cw5.Models;
 
-public class Doctor
-{
-    
+public class Doctor {
+    public int IdDoctor { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
 }

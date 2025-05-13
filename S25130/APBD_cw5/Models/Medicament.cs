2b7889992e77cee395ec4d693db3a5eba6e5c1c1
S25130 <s25130@pjwstk.edu.pl>
2025-05-13 19:58:07 +0200

    Update Medicament.cs

--- S25130/APBD_cw5/Models/Medicament.cs
+++ S25130/APBD_cw5/Models/Medicament.cs
@@ -1,6 +1,10 @@
 namespace APBD_cw5.Models;
 
-public class Medicament
-{
-    
+public class Medicament {
+    public int IdMedicament { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Type { get; set; } = null!;
+
+    public ICollection<PrescriptionMedicament> PrescriptionMedicaments { get; set; } = new List<PrescriptionMedicament>();
 }

1b33fe155db88020590332d4ca4785347355c59a
S25130 <s25130@pjwstk.edu.pl>
2025-05-13 19:57:42 +0200

    Update Patient.cs

--- S25130/APBD_cw5/Models/Patient.cs
+++ S25130/APBD_cw5/Models/Patient.cs
@@ -1,6 +1,10 @@
 namespace APBD_cw5.Models;
 
-public class Patient
-{
-    
+public class Patient {
+    public int IdPatient { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime BirthDate { get; set; }
+
+    public ICollection<Prescription> Prescriptions { get; set; } = new List<Prescription>();
 }

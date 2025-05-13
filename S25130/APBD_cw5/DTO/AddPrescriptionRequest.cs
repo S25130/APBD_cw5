ddc1efba1f526c27063e31c97491db48b08401f1
S25130 <s25130@pjwstk.edu.pl>
2025-05-13 20:46:51 +0200

    Update AddPrescriptionRequest.cs

--- S25130/APBD_cw5/DTO/AddPrescriptionRequest.cs
+++ S25130/APBD_cw5/DTO/AddPrescriptionRequest.cs
@@ -1,6 +1,25 @@
 namespace APBD_cw5.DTO;
 
-public class AddPrescriptionRequest
-{
-    
+public class AddPrescriptionRequest {
+    public PatientDto Patient { get; set; } = null!;
+    public DoctorDto Doctor { get; set; } = null!;
+    public List<PrescriptionMedicamentDto> Medicaments { get; set; } = new();
+    public DateTime Date { get; set; }
+    public DateTime DueDate { get; set; }
+}
+
+public class PatientDto {
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime BirthDate { get; set; }
+}
+
+public class DoctorDto {
+    public int IdDoctor { get; set; }
+}
+
+public class PrescriptionMedicamentDto {
+    public int IdMedicament { get; set; }
+    public int Dose { get; set; }
+    public string Description { get; set; } = null!;
 }

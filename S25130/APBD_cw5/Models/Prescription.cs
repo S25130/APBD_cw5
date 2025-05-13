5f6f6ea195288cabdc1c74747a66c47ba2b2928e
S25130 <s25130@pjwstk.edu.pl>
2025-05-13 19:58:49 +0200

    Update Prescription.cs

--- S25130/APBD_cw5/Models/Prescription.cs
+++ S25130/APBD_cw5/Models/Prescription.cs
@@ -1,6 +1,15 @@
 namespace APBD_cw5.Models;
 
-public class Prescription
-{
-    
+public class Prescription {
+    public int IdPrescription { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime DueDate { get; set; }
+
+    public int IdPatient { get; set; }
+    public Patient Patient { get; set; } = null!;
+
+    public int IdDoctor { get; set; }
+    public Doctor Doctor { get; set; } = null!;
+
+    public ICollection<PrescriptionMedicament> PrescriptionMedicaments { get; set; } = new List<PrescriptionMedicament>();
 }
